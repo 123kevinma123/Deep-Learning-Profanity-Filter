5a27565f8dbf1a18cec112d8d7b70eeef7c33998
123kevinma123 <123kevinma123@gmail.com>
2023-10-25 19:32:02 -0400

    fixed pytorch issues, updated code

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-class(nn.Module):
+class SimpleNN(nn.Module):
     def __init__(self):
-        super(SimpleNN, self).__init__()
-        self.fc = nn.Linear(2,1)
+        super(SimpleNN, self).__init()
+        self.fc = nn.Linear(2, 1)
 
     def forward(self, x):
-        return self.fc
-    
+        return self.fc(x)
+
 model = SimpleNN()
 
 criterion = nn.MSELoss()
-optimzer = optim.SGD(model.parameters(), lr = 0.01)
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+print("hello, world")

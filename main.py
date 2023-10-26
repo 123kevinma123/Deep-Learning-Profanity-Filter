704a83bf3d1d65f0d2fe375cb1582d85e126bdd7
123kevinma123 <123kevinma123@gmail.com>
2023-10-25 21:10:21 -0400

    successful installation of fasttext

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import site
+import fasttext
 
-class SimpleNN(nn.Module):
+"""class SimpleNN(nn.Module):
     def __init__(self):
         super(SimpleNN, self).__init()
         self.fc = nn.Linear(2, 1)
@@ -14,4 +16,5 @@ model = SimpleNN()
 
 criterion = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
-print("hello, world")
+print("hello, world")"""
+print(site.getsitepackages())
